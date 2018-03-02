cde3531977c62da57bfd30a13f3d72af788f95c6
gage langdon <gagelangdon@gmail.com>
2018-03-02 14:27:28 -0800

    cli time

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-(async function bingAlot() {
-	const number = process.argv[2] || 100;
-	const delay = process.argv[3] || 150;
-	for (let i = 0; i < number; i++) {
+#!/usr/bin/env node
+var program = require('commander');
+var chalk = require('chalk');
+
+(async function bing(count = 100, delay = 150) {
+	for (let i = 0; i < count; i++) {
 		await sleep(delay);
 		process.stdout.write('\x07');
+		console.log(chalk.red('bing'));
 	}
 })();
 
 async function sleep(ms) {
 	return new Promise(res => setTimeout(res, ms));
 }
+
+program.command('bingbingbingbingbing <count> <delay>').action(async (count, delay) => {
+	console.log(count, delay);
+	await bing(count, delay);
+});
+program.parse(process.argv);
