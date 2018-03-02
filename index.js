c40b72e7eb7d492a64d93949fdb1c9eb407abf7b
gage langdon <gagelangdon@gmail.com>
2018-03-02 14:40:53 -0800

    reduce

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 #!/usr/bin/env node
-var program = require('commander');
 var chalk = require('chalk');
 
-(async function bing(count = 100, delay = 150) {
+(async function bing(count = 1000, delay = 150) {
 	for (let i = 0; i < count; i++) {
 		await sleep(delay);
-		process.stdout.write('\x07');
-		console.log(chalk.red('bing'));
+		console.log(chalk.red('bing'), '\x07');
 	}
 })();
 
 async function sleep(ms) {
 	return new Promise(res => setTimeout(res, ms));
 }
-
-program.command('bingbingbing <count> <delay>').action(async (count, delay) => {
-	console.log(count, delay);
-	await bing(count, delay);
-});
-program.parse(process.argv);
